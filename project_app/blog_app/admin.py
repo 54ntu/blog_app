d331e3f3e4fdbc6482d8f02ea34d196558fae615
54ntu <chaudharysantaram8@gmail.com>
2023-06-09 12:19:10 +0545

    models & admin.py file is updated

--- project_app/blog_app/admin.py
+++ project_app/blog_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import blogModel
 
 # Register your models here.
+class blogAdminModel(admin.ModelAdmin):
+    list_display=('title','sub_title','desc')
+
+admin.site.register(blogModel,blogAdminModel)
--- project_app/blog_app/models.py
+++ project_app/blog_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class blogModel(models.Model):
+    title=models.CharField(max_length=255)
+    sub_title=models.CharField(max_length=255)
+    desc=models.TextField(null=False,blank=False)
+    blog_img=models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return f"{self.title}-{self.sub_title}-{self.desc}"
